e21f3a1c74c18e3a796e62effdf50ff54f773867
raniutami <raniran44@gmail.com>
2016-04-24 03:40:40 +0700

    update dashboard
    cuma ganti kata2

--- uilancer/resources/views/dashboard.blade.php
+++ uilancer/resources/views/dashboard.blade.php
@@ -156,7 +156,7 @@
 	<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">			
 		<div class="row">
 			<div class="col-lg-12">
-				<h1 class="page-header">Judul Halaman</h1>
+				<h1 class="page-header">Daftar Pekerjaan</h1>
 			</div>
 		</div><!--/.row-->		
 	</div><!--/.row-->
