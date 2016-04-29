27b5839a39c1eb4d7b804a80e4110f5047d77e4c
Muhammad Luthfi <luthfi.muhammad825@gmail.com>
2016-04-29 16:53:53 +0700

    Recommit

--- uilancer/app/Http/Controllers/PekerjaanController.php
+++ uilancer/app/Http/Controllers/PekerjaanController.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Pekerjaan;
-<<<<<<< HEAD
 use App\SkillTag;
 
 class PekerjaanController extends Controller
@@ -137,51 +136,3 @@ class PekerjaanController extends Controller
         }
     }
 }
-=======
-use App\SkillPekerjaan;
-
-class PekerjaanController extends Controller
-{
-    public function insert(Request $request) {
-
-    	$this->validate($request, [
-
-    			'judul' 	=> 'required|alpha_num|max:255',
-    			'deskripsi'	=> 'required',
-    			'budget'	=> 'required|numeric',
-    			'estimasi'	=> 'required',
-    			'deadline'	=> 'required|date|after:now',
-    			'skill'		=> 'required',
-
-    		]);
-
-    	$pekerjaan = new Pekerjaan;
-
-    	$pekerjaan->judul 		= $request->judul;
-    	$pekerjaan->deskripsi 	= $request->deskripsi;
-    	$pekerjaan->budget		= $request->budget;
-    	$pekerjaan->estimasi	= $request->estimasi;
-    	$pekerjaan->deadline	= $request->deadline;
-
-    	/* Disini perlu ada validasi terhadap jenis user
-    	   Kalau UI atau pemiliki akun resmi, maka 'isVerified' = 1 */
-    	$pekerjaan->isVerified 	= 1;
-
-    	$pekerjaan->isDone		= 0;
-    	$pekerjaan->isTaken		= 0;
-    	$pekerjaan->isClosed	= 0;
-
-    	$pekerjaan->save();
-
-    	$arrSkill = explode(';', $request->skill);
-    	foreach($arrSkill as $s){
-    		$skill = new SkillPekerjaan;
-    		$skill->skill = $s;
-    		
-    		$pekerjaan->skill()->save($skill);
-    	}
-
-    	return back();
-    }
-}
->>>>>>> post-lowongan-UI
