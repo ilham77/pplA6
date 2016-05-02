b5df972292017562267342ed186fb5a73b8aa045
ilham77 <77.ilham.77@gmail.com>
2016-05-02 10:41:25 +0700

    sedikit perubahan

--- uilancer/resources/views/search-dashboard.blade.php
+++ uilancer/resources/views/search-dashboard.blade.php
@@ -127,9 +127,9 @@
   <!-- Sidebar -->
   <div id="sidebar-collapse" class="col-sm-3 col-lg-2 sidebar">
     <ul class="nav menu">
-      <li><a href="dashboard"><span class="glyphicon glyphicon-user"></span> Profil</a></li>
+      <li><a href="{{url('dashboard')}}"><span class="glyphicon glyphicon-user"></span> Profil</a></li>
       <li><a href=""><span class="glyphicon glyphicon-list-alt"></span> Daftar Pekerjaan</a></li>
-      <li  class="active"><a href="search-dashboard"><span class="glyphicon glyphicon-search"></span> Cari Pekerjaan</a></li>
+      <li  class="active"><a href="{{url('search-dashboard')}}"><span class="glyphicon glyphicon-search"></span> Cari Pekerjaan</a></li>
       <li><a href="#"><span class="glyphicon glyphicon-pencil"></span> Buka Pekerjaan</a></li>
       <li class="parent ">
         <a href="#">
